a9a17502373dbd4bf683ebe9c3941079db8fb839
dmitry <oxpa97@mail.ru>
2025-08-29 12:51:25 +0300

    sidebar

--- src/widgets/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/Sidebar.tsx
@@ -1,46 +1,88 @@
-// Sidebar.tsx
-import React from "react"
+import React, { useMemo, useState } from "react"
 import styles from "./Sidebar.module.scss"
 import { logout } from "@shared/config/firebase"
 import { useAppNavigate } from "@shared/hooks/useAppNavigate"
 import { AppRoutes } from "@app/navigation/routes"
+import { useLocation } from "react-router-dom"
+import cn from "classnames"
+import Button from "@shared/Button/Button"
+import ArrowIcon from "@assets/icons/arrow-expand-left.svg?react"
+import LogoutIcon from "@assets/icons/logout.svg?react"
+import UserIcon from "@assets/icons/user.svg?react"
+import AppIcon from "@assets/icons/app.svg?react"
+import SupportIcon from "@assets/icons/support.svg?react"
+
+const routesNav = [
+	{
+		name: "Пользователи",
+		route: AppRoutes.users,
+		icon: <UserIcon />,
+	},
+	{
+		name: "Приложение",
+		route: AppRoutes.aplication,
+		icon: <AppIcon />,
+	},
+	{
+		name: "Поддержка",
+		route: AppRoutes.chats,
+		icon: <SupportIcon />,
+	},
+]
 
 const Sidebar: React.FC = () => {
 	const navigation = useAppNavigate()
+	const location = useLocation()
+	const [hidden, setHidden] = useState(false)
+
+	const currentRoute = useMemo(() => {
+		return location?.pathname
+	}, [location])
+
 	return (
-		<aside className={styles.sidebar}>
+		<aside
+			className={cn(styles.sidebar, {
+				[styles.hidden]: hidden,
+			})}
+		>
 			<div className={styles.header}>
 				<div
-					className={styles.adminName}
+					className={styles.title}
 					onClick={() => navigation(AppRoutes.main)}
 				>
-					Admins
+					{hidden ? "<A />" : "<Admin />"}
 				</div>
+
+				<button
+					className={styles.arrow}
+					onClick={() => setHidden(prev => !prev)}
+				>
+					<ArrowIcon />
+				</button>
 			</div>
 			<nav className={styles.nav}>
-				<button
-					className={styles.navItem}
-					onClick={() => navigation(AppRoutes.users)}
-				>
-					Пользователи
-				</button>
-				<button
-					className={styles.navItem}
-					onClick={() => navigation(AppRoutes.aplication)}
-				>
-					Приложение
-				</button>
-				<button
-					className={styles.navItem}
-					onClick={() => navigation(AppRoutes.chats)}
-				>
-					Поддержка
-				</button>
+				{routesNav.map(it => {
+					return (
+						<button
+							key={it.route}
+							className={cn(styles.navItem, {
+								[styles.navItemActive]: it.route === currentRoute,
+							})}
+							onClick={() => navigation(it.route)}
+						>
+							{it.icon}
+
+							<span>{it.name}</span>
+						</button>
+					)
+				})}
 			</nav>
 			<div className={styles.footer}>
-				<button className={styles.logoutBtn} onClick={logout}>
-					Выйти
-				</button>
+				<Button className={styles.logoutBtn} onClick={logout}>
+					<span>Выйти</span>
+
+					<LogoutIcon />
+				</Button>
 			</div>
 		</aside>
 	)
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 import path from "path"
+import svgr from "vite-plugin-svgr"
 
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		svgr({
+			include: "**/*.svg?react",
+			exclude: "",
+		}),
+	],
 	resolve: {
 		alias: {
 			"@assets": path.resolve(__dirname, "src/assets"),
